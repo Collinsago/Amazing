620a4835dfe322f0575486583e54b8b91820cf64
Collins Agofure <71197307+Collinsago@users.noreply.github.com>
2021-04-20 01:38:53 +0100

    Create AmazeQ.js

---
+++ AmazeQ.js
@@ -0,0 +1,160 @@
+
+//creating an array and passing the number, questions, options amd answers
+let MCQS = [
+
+    {
+        question: "In questions below, from One to Ten choose the word(s) or phrase which best fills the gap(s) <br><br> Jubril found that thieves had entered his house in his absence. He went to the police to report the... ",
+        answer:"break in",
+        options:[
+         "break out ",
+        "break up ",
+         "break in",
+        "break into",
+        ]        
+    },
+
+
+    {
+        question:"In adicot leaf, guard cellsdiffer fromother epidermal cells because they",
+        answer: "acknuclei",
+        options:[
+            "have no definite shape",
+            "acknuclei",
+            "aresmaller",
+            "lack vacuole.",
+        ]              
+    },
+    
+    {
+        question:"Which of the following structures is NOT found in the femaleagamalizard?",
+        answer:"Nasal scale",
+        options:[
+        "Nasal scale",
+        "Nuchalcrest.",
+        "Gularfold",
+        "Eardrum",
+
+        ]
+        
+    },
+    
+    {
+        question: "Herbs differ from shrubs because they",
+        answer:"are onlyperennials.",
+        options:[
+         "are useful to herbalists",
+        "are onlyannuals",
+         "are onlyperennials.",
+        "donot produce fruits",
+        ]        
+    },
+    {
+        question: "Ifan isolatedlivingcellis leftin distilledwater for two hours,itis likelyto",
+        answer: "lose some of itswater to the surrounding water",
+        options:[
+            "lose some of itswater to the surrounding water",
+           "loseall ofitswater to the surroundingwater",
+         "reproduce bybinaryfission",
+       "becomemore turgid.",
+        ]      
+        
+    },
+
+    {
+        question: "If an organic compound has its Hydrogen: Oxygen ratio as 2:1, it is likely to be",
+        answer:  "an aminoacid.",
+        options:[
+         "a protein",
+         "an aminoacid.",
+         "a carbohydrate,",
+        "a fat",
+        ]
+        
+        
+    },
+    {
+        question: "Which of the following elements are necessary for the formationofchlorophyll ina plant?",
+        answer: "Phosphorus and potassium.",
+        options:[
+        "Calciumandpotassium",
+        "Potassiumandsulphur",
+        "Calciumandsulphur",
+        "Phosphorus and potassium.",
+        ]
+        
+        
+    },
+    {
+        question: "Which of the following statements is NOT true of mammalianerythrocytes?",
+        answer:"They have nuclei at maturity.",
+        options:[
+            "They have haemoglobin",
+            "They have nuclei at maturity.",
+             "They have haemoglobin",
+            "The cells aremore numerous than leucocytes",
+        ]
+       
+        
+    },
+    {
+        question: "In woody plants, gases and water vapour are transported across the stems by the",
+        answer:"medullary fibres",
+        options:[
+             "medullary fibres",
+             "phloem fibres",
+             "phloem parenchyma",
+            "xylem fibres",
+        ]
+       
+        
+    },
+    {
+        question: "Which of the following substances isNOTfound in urine?",
+        answer: "Sodium chloride",
+        options:[
+          "Nitrogenous salts",
+         "Water",
+         "Calcium chloride",
+        "Sodium chloride",
+        ]
+        
+        
+    },
+    {
+        question: "The kidneys of all vertebrates act as osmo regulators. Thismeans that they",
+        answer:"Control the volume of blood entering the kidneys",
+        options:[
+            "keep the composition of the plasma constant",
+        "regulate osmoticprocesses",
+         "Control the volume of blood entering the kidneys",
+         "increase the osmotic pressure of blood.",
+        ]
+        
+        
+    },
+
+    {
+        question:"Which of the following structures is NOT found in the femaleagamalizard?",
+        answer:"Nasal scale",
+        options:[
+        "Nasal scale",
+        "Nuchalcrest.",
+        "Gularfold",
+        "Eardrum",
+
+        ]
+        
+    },
+    
+    {
+        question: "Herbs differ from shrubs because they",
+        answer:"are onlyperennials.",
+        options:[
+         "are useful to herbalists",
+        "are onlyannuals",
+         "are onlyperennials.",
+        "donot produce fruits",
+        ]        
+    },
+   
+];
